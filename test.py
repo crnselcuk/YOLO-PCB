6025f335ebad25385e09cfe022c599e189a94eb6
crnselcuk <157848120+crnselcuk@users.noreply.github.com>
2026-04-20 23:37:17 +0300

    Update test.py

--- test.py
+++ test.py
@@ -23,7 +23,7 @@ def test(data,
          weights=None,
          batch_size=32,
          imgsz=608,
-         conf_thres=0.01,
+         conf_thres=0.25,
          iou_thres=0.01,  # for NMS
          save_json=False,
          single_cls=False,
@@ -90,7 +90,7 @@ def test(data,
                                        prefix=colorstr('test: ' if opt.task == 'test' else 'val: '))[0]
 
     seen = 0
-    confusion_matrix = ConfusionMatrix(nc=nc, conf=0.2, iou_thres=0.02) #DUZENLEME
+    confusion_matrix = ConfusionMatrix(nc=nc, conf=0.25, iou_thres=0.01) #DUZENLEME
     names = {k: v for k, v in enumerate(model.names if hasattr(model, 'names') else model.module.names)}
     coco91class = coco80_to_coco91_class()
     s = ('%20s' + '%12s' * 6) % ('Class', 'Images', 'Targets', 'P', 'R', 'mAP@.5', 'mAP@.5:.95')
